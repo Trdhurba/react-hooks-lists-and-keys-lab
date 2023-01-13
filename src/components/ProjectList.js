d9af179c06d7af8e7afa0a8afba47dbf506494e4
dhurbaraj timilsina <trdhu@david>
2023-01-13 08:58:53 -0800

    Navbar.js,ProjectItem.js,Projectlist.js,react-hooks-listsand-keys-lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,14 @@
+import userEvent from "@testing-library/user-event";
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
-  return (
+  console.log("this is the prop of  projects",projects);
+  const ProjectItems= projects.map(project=> <ProjectItem technologies= {project.technologies }name= {project.name}about= {project.about} key= {project.id} />)
+  return ( 
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{ProjectItems}</div>
     </div>
   );
 }
